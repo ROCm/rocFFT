9640a9fb65028571cbfb14923c3f4b8f903567f7
Steve Leung <Steve.Leung@amd.com>
2025-05-02 11:27:37 -0600

    a couple of lengths are better with wgs 512

--- library/src/device/kernel-generator.py
+++ library/src/device/kernel-generator.py
@@ -702,9 +702,9 @@ def list_small_kernels():
         NS(length=4096, workgroup_size=256, threads_per_transform=256, factors=(16, 16, 16), runtime_compile=True),
         NS(length=4704, workgroup_size=256, threads_per_transform=224, factors=(8, 4, 7, 7, 3), double_precision=False, runtime_compile=True),
         NS(length=5488, workgroup_size=256, threads_per_transform=196, factors=(7, 4, 7, 4, 7), double_precision=False, runtime_compile=True),
-        NS(length=6144, workgroup_size=256, threads_per_transform=192, factors=(8, 4, 4, 6, 8), double_precision=False, runtime_compile=True),
+        NS(length=6144, workgroup_size=512, threads_per_transform=512, factors=(16, 4, 8, 3, 4), double_precision=False, runtime_compile=True),
         NS(length=6561, workgroup_size=256, threads_per_transform=243, factors=(3, 3, 3, 3, 3, 3, 3, 3), double_precision=False, runtime_compile=True),
-        NS(length=8192, workgroup_size=256, threads_per_transform=256, factors=(8, 8, 8, 4, 4), double_precision=False, runtime_compile=True),
+        NS(length=8192, workgroup_size=512, threads_per_transform=512, factors=(16, 4, 4, 4, 8), double_precision=False, runtime_compile=True),
     ]
 
     kernels = [NS(**kernel.__dict__,
