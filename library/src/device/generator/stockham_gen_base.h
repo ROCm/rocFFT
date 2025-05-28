0b7f232b8407849a9214860b7626745302b8f6ad
Steve Leung <Steve.Leung@amd.com>
2025-05-28 14:15:19 -0600

    generator: loop if necessary for each FFT dim in 2D_SINGLE
    Previously, workgroup size for 2D_SINGLE kernels was chosen such that all
    FFTs in each dimension could be done with a single workgroup.  But this
    becomes infeasible as dimensions get larger.
    
    So instead, loop in the generator to perform enough FFTs for each
    dimension using the available threads.  As a side effect this means the
    extra FFT for even-length real-complex can be done by existing threads
    rather than making the first N threads do it while the rest wait.

--- library/src/device/generator/stockham_gen_base.h
+++ library/src/device/generator/stockham_gen_base.h
@@ -70,8 +70,9 @@ struct StockhamKernel : public StockhamGeneratorSpecs
             workgroup_size = threads_per_transform * transforms_per_block;
         }
 
-        nregisters = compute_nregisters(length, factors, threads_per_transform);
-        R.size     = Expression{nregisters};
+        nregisters                = compute_nregisters(length, factors, threads_per_transform);
+        R.size                    = Expression{nregisters};
+        lds_reg_sync.decl_default = Literal{"true"};
     }
     virtual ~StockhamKernel(){};
 
@@ -210,6 +211,9 @@ struct StockhamKernel : public StockhamGeneratorSpecs
     // butterfly registers
     Variable R{"R", "scalar_type", false, false};
 
+    // do syncthreads in lds_reg device functions
+    Variable lds_reg_sync{"lds_reg_sync", "bool"};
+
     virtual std::vector<unsigned int> launcher_lengths()
     {
         return {length};
@@ -224,6 +228,7 @@ struct StockhamKernel : public StockhamGeneratorSpecs
         TemplateList tpls;
         tpls.append(scalar_type);
         tpls.append(stride_type);
+        tpls.append(lds_reg_sync);
         return tpls;
     }
 
@@ -574,7 +579,7 @@ struct StockhamKernel : public StockhamGeneratorSpecs
         // first pass of load (full)
         unsigned int width  = factors[0];
         float        height = static_cast<float>(length) / width / threads_per_transform;
-        body += SyncThreads();
+        body += If{lds_reg_sync, {SyncThreads()}};
         body += add_work(std::bind(load_lds, this, _1, _2, _3, _4, _5, Component::BOTH),
                          width,
                          height,
@@ -604,7 +609,7 @@ struct StockhamKernel : public StockhamGeneratorSpecs
         unsigned int width     = factors.back();
         float        height    = static_cast<float>(length) / width / threads_per_transform;
         unsigned int cumheight = product(factors.begin(), factors.end() - 1);
-        body += SyncThreads();
+        body += If{lds_reg_sync, {SyncThreads()}};
         body += add_work(std::bind(store_lds, this, _1, _2, _3, _4, _5, Component::BOTH, cumheight),
                          width,
                          height,
@@ -899,9 +904,10 @@ struct StockhamKernel : public StockhamGeneratorSpecs
 
     virtual StatementList store_to_global(bool store_registers) = 0;
 
-    virtual TemplateList device_lds_reg_inout_device_call_templates()
+    virtual TemplateList device_lds_reg_inout_device_call_templates(bool syncthreads = true)
     {
-        return {scalar_type, stride_type};
+        Variable sync_var{syncthreads ? "true" : "false", "bool"};
+        return {scalar_type, stride_type, sync_var};
     }
 
     virtual std::vector<Expression> device_lds_reg_inout_device_call_arguments()
